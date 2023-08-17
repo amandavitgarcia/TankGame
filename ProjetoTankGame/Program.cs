5dc5d1b203e660d2df77d055d03893cc94ec355d
Amanda <amandavitgarcia@gmail.com>
2023-08-17 02:02:31 -0300

    Initial Commit

---
+++ ProjetoTankGame/Program.cs
@@ -0,0 +1,115 @@
+﻿using ProjetoTankGame;
+
+//Verificando se o nome digitado pelo usuário está na lista de tanques 
+Tank CheckIfTankExists(string name, List<Tank> customTanksList)
+{
+    foreach (var tank in customTanksList)
+    {
+        if (tank.Name.ToUpper() == name.ToUpper())
+        {
+            return tank;
+        }
+    }
+
+    return null;
+}
+
+//Criação dos tanques
+var customTanksList = new List<Tank>();
+
+int numberTanks = 0;
+
+while(numberTanks < 2 || numberTanks > 10)
+{
+    Console.WriteLine("------------Tank Battle ------------");
+    Console.WriteLine("\nInforme quantos tanques vão duelar \n -- Mín  2 & Máx 10 --");
+    Console.WriteLine("\nRespeite os limites de quantidades mínima e máxima. ");
+    
+    
+    //Tratando algumas exceções
+    try
+    {
+        numberTanks = int.Parse(Console.ReadLine());
+    }
+    catch(FormatException exception)
+    {
+        Console.WriteLine("\nO valor digitado não corresponde ao solicitado.\n");
+    }
+    catch (ArgumentNullException exception)
+    {
+        Console.WriteLine("\nNenhum valor foi informado.\n");
+    }
+}
+
+//Incluindo os tanques digitados pelo usuário na lista de tanques
+int tankCounter = 1;
+
+while(tankCounter - 1 != numberTanks)
+{
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine($"\nDigite o nome do: {tankCounter}° tanque");
+    string nome = Console.ReadLine();
+    var newTank = new Tank(nome);
+    customTanksList.Add(newTank);
+    tankCounter += 1;
+}
+
+//Iniciando rodadas com um tanque gerado aleatóriamente
+int roundCounter = 1;
+
+while (customTanksList.Count != 1)
+{
+    Console.WriteLine($"\n \n \n \n-----------------------------------");
+    Console.WriteLine($"\nComeçando {roundCounter}ª rodada...");
+    Console.WriteLine($"\nQuantidade de Jogadores: {customTanksList.Count}");
+
+    Console.WriteLine("\nSorteando quem vai atirar...");
+
+
+    Random rnd = new Random();
+    int randomNumber = rnd.Next(0, customTanksList.Count);
+    var sniperTank = customTanksList[randomNumber];
+    Console.WriteLine($"\nO tanque sorteado foi: {sniperTank.Name}");
+
+    bool isTankSelected = false;
+
+    while (!isTankSelected)
+    {
+        Console.WriteLine("\nLista de alvos disponiveis:");
+
+        //print dos tanques pra atirar
+        foreach (var tank in customTanksList)
+        {
+            if (tank != sniperTank)
+            {
+                Console.WriteLine($"{tank.Name}");
+            }
+        }
+        Console.WriteLine("\nQual tanque será atingido? \n");
+        string name = Console.ReadLine();
+
+        //Validando o tanque que será atingido
+        var tankDamaged = CheckIfTankExists(name, customTanksList);
+        if(tankDamaged != null)
+        {
+            sniperTank.FireAt(tankDamaged);
+
+            //Removendo o tanque com a blindagem zerada
+            if (!tankDamaged.Alive)
+            {
+                customTanksList.Remove(tankDamaged);
+            }
+
+            roundCounter++;
+            isTankSelected = true;
+        }
+        else
+        {
+            Console.WriteLine($"\nTanque Selecionado Inválido");
+        }
+    }
+}
+//Informando o tanque vencedor pegando o primeiro (e unico) que sobrou da lista de tanques
+Console.WriteLine($"\nO vencedor é {customTanksList.First().Name}");
+Console.WriteLine("");
+Console.WriteLine("Fim");
---
+++ ProjetoTankGame/Tank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTankGame
+{
+    //Classe Tank com seus métodos e atributos
+    public class Tank
+    {
+        public string Name { get; set; }
+        public bool Alive { get; set; }
+        public int Ammo { get; set; }
+        public int Armor { get; set; }
+
+        //Construtor - onde inicializamos as propiedades
+        public Tank(string name)
+        {
+            Name = name;
+            Alive = true;
+            Ammo = 5;
+            Armor = 60;
+        }
+
+        //Métodos da classe Tank
+        public void FireAt(Tank enemy)
+        {
+            if (enemy.Ammo >= 1)
+            {
+                enemy.Ammo -= 1;
+                Console.WriteLine($"{Name} fires on {enemy.Name}\n");
+                Console.WriteLine("-----------------------------------");
+
+                enemy.Hit();
+
+                ShowInfo();
+                enemy.ShowInfo();
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name}, has no shells!");
+            }
+        }
+
+        public void Hit()
+        {
+            this.Armor -= 20;
+            Console.WriteLine($"{this.Name} is hit");
+
+            if (this.Armor <= 0)
+            {
+                this.Explode();
+            }
+        }
+
+        public void Explode()
+        {
+            this.Alive = false;
+            Console.WriteLine($"{this.Name}, EXPLODESSS!");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\n{this.Name.ToUpper()} \nAmmo: {this.Ammo} \nArmor:{this.Armor}");
+        }
+
+        static void Exit()
+        {
+            Console.WriteLine("Finished game");
+
+            Thread.Sleep(4000);
+            System.Environment.Exit(0);
+        }
+    }
+}
+
